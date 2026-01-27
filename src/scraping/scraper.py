a0ffe5fed903174bd913f504406ececf029f54b5
turchinigor <turchin.igor@gmail.com>
2026-01-28 00:20:37 +0200

    fix scrap url

--- src/scraping/scraper.py
+++ src/scraping/scraper.py
@@ -5,15 +5,19 @@ import random
 from datetime import datetime
 import pandas as pd
 from pathlib import Path
-from .utils import scrap_url
+from .utils import scrap_page, next_page
 from ..io import ensure_dir, save_json
 import yaml
+
+#TODO: Add function to read file per real estate type if exists
+#TODO: Save listings after each page
+#TODO: Implement fall back in case the script fails, to start where it stoped
+#TODO: Add all of it to one function only
 
 # Load config
 config_path = Path(__file__).parent.parent.parent / "config" / "config.yaml"
 with open(config_path, 'r') as f:
     config = yaml.safe_load(f)
-
 
 #Output Path
 PATH_OUT = Path(__file__).parent.parent.parent / "data" / "tests"
@@ -22,8 +26,6 @@ ensure_dir(PATH_OUT)
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.delete_all_cookies()
-
-
 
 #Base URL for Appartments
 URL_AP = config['urls']['apartments']
@@ -36,9 +38,20 @@ URL_LAND = config['urls']['land']
 
 driver.get(URL_AP)
 
-#TODO: Fix scrap_url / Reads the wrapper, but returns an empty list
-listings = scrap_url(driver=driver, url=URL_AP)
+def scrap_urls(driver, base_url, pages):
+    page_availability = True
+    p = 1
+    listings = []
+    driver.get(base_url)
+    while page_availability and p <= pages:
+        listings_page = scrap_page(driver, p)
+        listings.extend(listings_page)
+        page_availability = next_page(driver)
+        p += 1
+    return listings
+listings = scrap_urls(driver=driver, base_url=URL_AP, pages=6)
 save_json(Path(PATH_OUT, "listings.json"), listings=listings)
 
 driver.close()
 driver.quit()
+
--- src/scraping/utils.py
+++ src/scraping/utils.py
@@ -1,9 +1,11 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
+import random
 
 
-def scrap_element(element_type, wrapper, identifier: str, extract_type: str = "text") -> str | None:
+def scrap_element_url(element_type, wrapper, identifier: str, extract_type: str = "text") -> str | None:
     try:
         elem = wrapper.find_element(element_type, identifier)
         if extract_type == "text":
@@ -14,19 +16,22 @@ def scrap_element(element_type, wrapper, identifier: str, extract_type: str = "t
         print(f"Error extracting {extract_type}: {e}")
         return None
 
-def scrap_url(driver, url: str) -> list[dict]:
-    driver.get(url)
-    
+def scrap_page(driver, page: int) -> list[dict]:
+
+    time.sleep(1 + random.uniform(0.1, 0.3))
+
+    wrapper_selector = "[class^='AdShort_wrapper__']"
+
     # Wait for the page to load (adjust selector as needed)
     try:
-        WebDriverWait(driver, 30).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "[class^='AdShort_wrapper__']"))
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, wrapper_selector))
         )
+        print("Successfully found wrapper")
     except Exception as e:
         print(f"Timeout waiting for elements: {e}")
         return []  # Return empty if failed
     
-    wrapper_selector = "[class^='AdShort_wrapper__']"
     url_identifier_class = "[class^='AdShort_title__link__']"
     square_m2_class = "[class^='AdShort_distance__']"
     price_class = "[class^='AdShort_price__']"
@@ -34,27 +39,28 @@ def scrap_url(driver, url: str) -> list[dict]:
     
     # Find all wrapper elements (each represents one listing)
     wrappers = driver.find_elements(By.CSS_SELECTOR, wrapper_selector)
-    
     listings = []
     for i, wrapper in enumerate(wrappers):
         try:
             # Extract elements within each wrapper
-            listing_url = scrap_element(element_type=By.CSS_SELECTOR, wrapper=wrapper, identifier=url_identifier_class, extract_type="href")
-            description = scrap_element(element_type=By.CSS_SELECTOR, wrapper=wrapper, identifier=url_identifier_class, extract_type="text")
+            listing_url = scrap_element_url(element_type=By.CSS_SELECTOR, wrapper=wrapper, identifier=url_identifier_class, extract_type="href")
+
+            description = scrap_element_url(element_type=By.CSS_SELECTOR, wrapper=wrapper, identifier=url_identifier_class, extract_type="text")
             
-            square_m2 = scrap_element(element_type=By.CSS_SELECTOR, wrapper=wrapper, identifier=square_m2_class, extract_type="text")
+            square_m2 = scrap_element_url(element_type=By.CSS_SELECTOR, wrapper=wrapper, identifier=square_m2_class, extract_type="text")
             
-            price = scrap_element(element_type=By.CSS_SELECTOR, wrapper=wrapper, identifier=price_class, extract_type="text")
+            price = scrap_element_url(element_type=By.CSS_SELECTOR, wrapper=wrapper, identifier=price_class, extract_type="text")
             
-            date = scrap_element(element_type=By.CSS_SELECTOR, wrapper=wrapper, identifier=date_class, extract_type="text")
+            date = scrap_element_url(element_type=By.CSS_SELECTOR, wrapper=wrapper, identifier=date_class, extract_type="text")
             
             # Store in a dict
             listing = {
+                "page": page,
                 "url": listing_url,
                 "description": description,
                 "square_m2": square_m2,
                 "price": price,
-                "date": date
+                "date": date,
             }
             listings.append(listing)
         except Exception as e:
@@ -66,6 +72,14 @@ def scrap_url(driver, url: str) -> list[dict]:
     
     return listings
 
+def next_page(driver):
+    next_button = "[class^='Pagination_pagination__container__buttons__wrapper__icon__next']"
+    try:
+        n_elem = driver.find_element(By.CSS_SELECTOR, next_button)
+        n_elem.click()
+        return True
+    except Exception as e:
+        return False
 
 # map_url_class = "adPage__content__map-redirect"
 # ^ get.attribute("href")
