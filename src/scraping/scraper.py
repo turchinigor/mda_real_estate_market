bea2eae9cc13da9b7aad05abfc0f71610d8bf265
Igor Turchin <turchin@unhcr.org>
2026-02-20 15:06:54 +0200

    fix iterate_listings formula

--- src/scraping/scraper.py
+++ src/scraping/scraper.py
@@ -9,7 +9,7 @@ import logging
 import yaml
 
 from .utils import scrap_urls, get_all_properties
-from ..io import ensure_dir, save_json
+from ..io import ensure_dir, save_json, read_json
 from ..logging_config import setup_logging, get_logger
 
 # Seting up logging
@@ -30,19 +30,27 @@ def scrap_urls_pipeline(listing_types: dict, scrap_classes: dict, path: Path) ->
 
 ### Listing Scraping pipeline
 
-def iterate_listings(driver, urls_data, config_elem, history, new_file):
-    urls_df = pd.DataFrame(urls_data)
-    done_df = pd.DataFrame(history)
-    remaining = urls_df[~urls_df["url"].isin(done_df["url"])]
-    listings = []
-    for i, row in remaining.iterrows():
+def iterate_listings(driver, urls_data, config_elem, history):
+    history_json = read_json(history)
+    if len(history_json) > 0:
+        listings = history_json
+    else:
+        listings = []
+
+    done = [u["url"] for u in history_json]
+    remaining = [r for r in urls_data if r["url"] not in done]
+    
+    for i, row in enumerate(remaining, start=1):
         if i % 20 == 0:
-            save_json(new_file, data=listings)
+            save_json(history, data=listings)
         driver.get(row["url"])
         time.sleep(random.uniform(1,2))
         properties = get_all_properties(driver=driver, row=row, scrap_elements=config_elem)
         if properties is None:
-            logger.error("Listing skipped, missing information. URL: ", row["url"])
+            logger.error("Listing skipped, missing information. URL: %s", row["url"])
+            continue
+        listings.append(properties)
+    save_json(history, data=listings)
         
 
 
@@ -50,6 +58,3 @@ def scrap_listing():
     driver = webdriver.Chrome()
     driver.maximize_window()
     driver.delete_all_cookies()
-
-
-    
